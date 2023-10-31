5912d22fbc33f948e724ce31c01f78724a332d24
Rivera TI <ti_rivera@hpolicial.local>
2023-10-31 11:37:24 -0300

    start about section

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,3 @@
-<div class="flex flex-col space-y-4 p-5 items-start rounded-md shadow-md text-slate-700 bg-slate-50 opacity-90">
+<div {{ $attributes->class(['flex flex-col space-y-4 p-5 items-start rounded-md shadow-md text-slate-700 bg-slate-50 opacity-90'])}} >
         {{ $slot }}
 </div>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -37,22 +37,10 @@
         
     </div>
 
-    <div class="text-4xl max-w-7xl mx-auto h-screen items-center">
-        <div class="">
-            test
-        </div>
-        <x-card>
+    <div class="text-4xl max-w-7xl mx-auto h-screen items-center pt-20">
+        <x-card class="bg-amber-300">
             <p class="text-gray-700 text-sm w-[85rem]">
-                Welcome to my personal web development corner, where I embark on a creative odyssey through the fascinating realm of the web. Here, I'll be sharing my passion for crafting elegant and efficient web solutions, from front-end magic to back-end wizardry. 
-                Join me as I explore the ever-evolving landscape of technology, one line of code at a time, and let's build the digital future together.
-            </p>
-            <p class="text-gray-700 text-sm w-[85rem]">
-                I'll be diving into the world of web development with a strong focus on crafting dynamic and visually appealing web experiences. 
-                My toolkit includes an array of exciting technologies such as <span class='font-semibold text-black'>HTML/CSS</span> for structure 
-                and styling, the interactivity of <span class='font-semibold text-black'>JavaScript</span>, the versatility of 
-                <span class='font-semibold text-black'>React</span>, and the robust back-end power of <span class='font-semibold text-black'>PHP</span> 
-                and <span class='font-semibold text-black'>Laravel</span>. Join me on this thrilling journey as we transform ideas into stunning 
-                digital reality and create web pages that are both beautiful and functional.
+                {{--  --}}
             </p>
         </x-card>
     </div>
