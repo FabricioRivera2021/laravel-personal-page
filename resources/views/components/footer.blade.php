4261cfc0d4a437e9d22b9e02f0431cee8f3f3946
Rivera TI <ti_rivera@hpolicial.local>
2023-11-01 16:25:53 -0300

    añadiendo cosas, navbar footer etc

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,31 +1,40 @@
 <footer class="bg-slate-600">
-    <div class="h-48 bg-slate-600 flex space-x-10 justify-between p-10 text-slate-300 text-sm w-[80rem] mx-auto">
+    <div class="h-48 bg-slate-600 flex space-x-10 justify-between items-end pb-4 text-slate-300 text-sm w-[80rem] mx-auto">
         <div>
-            <ul>
-                <li class="mb-2">item 1</li>
-                <li class="mb-2">item 2</li>
-                <li class="mb-2">item 3</li>
+            <ul class="font-thin">
+                    <li class="mb-2">
+                        <a href="">
+                            English version
+                        </a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="">
+                            Version en español
+                        </a>
+                    </li>
             </ul>
         </div>
         <div>
             <ul>
-                <li class="mb-2">item 1</li>
-                <li class="mb-2">item 2</li>
-                <li class="mb-2">item 3</li>
+                    <li class="mb-2">Home</li>
+                    <li class="mb-2">Projects</li>
+                    <li class="mb-2">Contact form</li>
+                    <li class="mb-2">Blog</li>
             </ul>
         </div>
         <div>
             <ul>
-                <li class="mb-2">item 1</li>
-                <li class="mb-2">item 2</li>
-                <li class="mb-2">item 3</li>
+                <li class="mb-2 font-semibold">Contact information:</li>
+                <li class="mb-2">Fabricio.Rivera2012@gmail.com</li>
+                <li class="mb-2">github/fabricioRivera</li>
+                <li class="mb-2">linkedin/fabricioRivera</li>
             </ul>
         </div>
         <div>
-            <ul>
-                <li class="mb-2">item 1</li>
-                <li class="mb-2">item 2</li>
-                <li class="mb-2">item 3</li>
+            <ul class="text-right">
+                <li class="mb-2">Made with Laravel by Fabricio Rivera</li>
+                <li class="mb-2">Montevideo, Uruguay</li>
+                <li class="mb-2">&copy; 2023</li>
             </ul>
         </div>
     </div>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -46,12 +46,17 @@
     <div class="h-screen bg-gradient-to-r from-sky-200 to-indigo-700">
         <div class="text-3xl max-w-7xl mx-auto h-[calc(100vh-40px)] grid grid-rows-3 grid-flow-col gap-4 pt-16">
             <x-card class="row-span-3">
-                <h3 class="max-w-[60rem] font-bold text-slate-500">
-                    ABOUT ME
-                    <span class="block text-lg text-orange-400 font-thin">
-                        Story time
-                    </span>
-                </h3>
+                <div class="flex justify-start items-center space-x-3 text-slate-500">
+                    <h3 class="max-w-[60rem] font-bold">
+                        ABOUT ME
+                    </h3>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                        <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clip-rule="evenodd" />
+                    </svg>                      
+                </div>
+                <span class="block text-lg text-orange-400 font-thin">
+                    Story time
+                </span>
                 <p class="text-gray-700 text-sm">
                     Welcome to my personal web development corner, where I embark on a creative odyssey through the fascinating realm of the web. Here, I'll be sharing my passion for crafting elegant and efficient web solutions, from front-end magic to back-end wizardry. 
                     Join me as I explore the ever-evolving landscape of technology, one line of code at a time, and let's build the digital future together.
@@ -66,24 +71,37 @@
                 </p>
             </x-card>
             <x-card>
-                <h3 class="max-w-[60rem] min-w-[20rem] font-bold text-slate-500">
-                    ACADEMICS
-                    <span class="block text-lg text-orange-400 font-thin">
-                        ... what have i studied
-                    </span>
-                </h3>
+                <div class="flex justify-start items-center space-x-3 text-slate-500">
+                    <h3 class="max-w-[60rem] font-bold">
+                        ACADEMICS
+                    </h3>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                        <path d="M11.7 2.805a.75.75 0 01.6 0A60.65 60.65 0 0122.83 8.72a.75.75 0 01-.231 1.337 49.949 49.949 0 00-9.902 3.912l-.003.002-.34.18a.75.75 0 01-.707 0A50.009 50.009 0 007.5 12.174v-.224c0-.131.067-.248.172-.311a54.614 54.614 0 014.653-2.52.75.75 0 00-.65-1.352 56.129 56.129 0 00-4.78 2.589 1.858 1.858 0 00-.859 1.228 49.803 49.803 0 00-4.634-1.527.75.75 0 01-.231-1.337A60.653 60.653 0 0111.7 2.805z" />
+                        <path d="M13.06 15.473a48.45 48.45 0 017.666-3.282c.134 1.414.22 2.843.255 4.285a.75.75 0 01-.46.71 47.878 47.878 0 00-8.105 4.342.75.75 0 01-.832 0 47.877 47.877 0 00-8.104-4.342.75.75 0 01-.461-.71c.035-1.442.121-2.87.255-4.286A48.4 48.4 0 016 13.18v1.27a1.5 1.5 0 00-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.661a6.729 6.729 0 00.551-1.608 1.5 1.5 0 00.14-2.67v-.645a48.549 48.549 0 013.44 1.668 2.25 2.25 0 002.12 0z" />
+                        <path d="M4.462 19.462c.42-.419.753-.89 1-1.394.453.213.902.434 1.347.661a6.743 6.743 0 01-1.286 1.794.75.75 0 11-1.06-1.06z" />
+                    </svg>
+                </div>
+                <p class="block text-lg text-orange-400 font-thin">
+                    ... what have i studied
+                </p>
                 <p class="text-gray-700 text-sm ">
                     Welcome to my personal web development corner, where I embark on a creative odyssey through the fascinating realm of the web. Here, I'll be sharing my passion for crafting elegant and efficient web solutions, from front-end magic to back-end wizardry. 
                     Join me as I explore the ever-evolving landscape of technology, one line of code at a time, and let's build the digital future together.
                 </p>
             </x-card>
             <x-card>
-                <h3 class="max-w-[60rem] min-w-[20rem] font-bold text-slate-500">
-                    EXPERIENCE
-                    <span class="block text-lg text-orange-400 font-thin">
-                        ... a little background
-                    </span>
-                </h3>
+                <div class="flex justify-start items-center space-x-3 text-slate-500">
+                    <h3 class="max-w-[60rem] font-bold">
+                        EXPERIENCE
+                    </h3>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                        <path fill-rule="evenodd" d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0016.5 9h-1.875a1.875 1.875 0 01-1.875-1.875V5.25A3.75 3.75 0 009 1.5H5.625zM7.5 15a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 017.5 15zm.75 2.25a.75.75 0 000 1.5H12a.75.75 0 000-1.5H8.25z" clip-rule="evenodd" />
+                        <path d="M12.971 1.816A5.23 5.23 0 0114.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 013.434 1.279 9.768 9.768 0 00-6.963-6.963z" />
+                    </svg>                                           
+                </div>
+                <p class="block text-lg text-orange-400 font-thin">
+                    ... a little background
+                </p>
                 <ol class="text-sm">
                     <li>
                         <h4 class="text-lg font-semibold">item 1</h4>
@@ -100,12 +118,17 @@
                 </ol>
             </x-card>
             <x-card>
-                <h3 class="max-w-[60rem] min-w-[20rem] font-bold text-slate-500">
-                    TECHNOLOGIES
-                    <span class="block text-lg text-orange-400 font-thin">
-                        ... that i have used
-                    </span>
-                </h3>
+                <div class="flex justify-start items-center space-x-3 text-slate-500">
+                    <h3 class="max-w-[60rem] font-bold">
+                        TECHNOLOGIES
+                    </h3>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                        <path fill-rule="evenodd" d="M12 1.5a.75.75 0 01.75.75V4.5a.75.75 0 01-1.5 0V2.25A.75.75 0 0112 1.5zM5.636 4.136a.75.75 0 011.06 0l1.592 1.591a.75.75 0 01-1.061 1.06l-1.591-1.59a.75.75 0 010-1.061zm12.728 0a.75.75 0 010 1.06l-1.591 1.592a.75.75 0 01-1.06-1.061l1.59-1.591a.75.75 0 011.061 0zm-6.816 4.496a.75.75 0 01.82.311l5.228 7.917a.75.75 0 01-.777 1.148l-2.097-.43 1.045 3.9a.75.75 0 01-1.45.388l-1.044-3.899-1.601 1.42a.75.75 0 01-1.247-.606l.569-9.47a.75.75 0 01.554-.68zM3 10.5a.75.75 0 01.75-.75H6a.75.75 0 010 1.5H3.75A.75.75 0 013 10.5zm14.25 0a.75.75 0 01.75-.75h2.25a.75.75 0 010 1.5H18a.75.75 0 01-.75-.75zm-8.962 3.712a.75.75 0 010 1.061l-1.591 1.591a.75.75 0 11-1.061-1.06l1.591-1.592a.75.75 0 011.06 0z" clip-rule="evenodd" />
+                    </svg>                 
+                </div>
+                <span class="block text-lg text-orange-400 font-thin">
+                    ... that i have used
+                </span>
                 <div class="flex space-x-4 flex-wrap">
                     <div class="h-16 w-16 bg-slate-300 rounded-md"></div>
                     <div class="h-16 w-16 bg-slate-200 rounded-md"></div>
