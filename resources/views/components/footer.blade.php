5ef399914a292b3a4b32aff6b791e540474d89d0
Rivera TI <ti_rivera@hpolicial.local>
2023-11-03 10:48:32 -0300

    nav color change

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
-<footer class="bg-slate-800">
+<footer class="bg-slate-700">
     <div class="h-[3px] w-full bg-slate-400"></div>
-    <div class="h-48 bg-slate-800 flex space-x-10 justify-between items-end pb-10 text-slate-300 text-xs max-w-[95rem] mx-auto">
+    <div class="h-48 bg-slate-700 flex space-x-10 justify-between items-end pb-10 text-slate-300 text-xs max-w-[95rem] mx-auto">
         <div>
             <ul>
                 <li class="mb-2">
