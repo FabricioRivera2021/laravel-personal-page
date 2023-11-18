c2775d4417ff9bd121b3204683f221a6eae17659
unknown <fabricio.rivera2012@gmail.com>
2023-11-17 21:09:29 -0300

    trabajando en el contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,8 +1,7 @@
 <x-nav-bar />
 
 <x-layout>
-    <div class="h-screen bg-slate-100 bg-contact-background bg-cover bg-center bg-opacity-50">
-        <div class="w-full h-screen bg-slate-200 absolute opacity-20"></div>
+    <div class="h-screen bg-slate-100 bg-background bg-cover bg-center">
         <div class="text-xl w-full mx-auto h-[calc(100vh-40px)] flex justify-evenly items-center opacity-95 z-50">
 
             <div class="flex flex-col items-center justify-center p-10 bg-slate-50 rounded-md shadow-md w-1/2">
@@ -11,25 +10,19 @@
                 <div class="flex w-full">
                     <div class="flex space-x-2 items-center w-full px-4">
 
-                        <div class="w-full h-[92%] bg-slate-300 text-sm flex flex-col justify-between items-center m-5">
+                        <div class="max-w-[35%] h-[92%] bg-slate-300 text-sm flex flex-col justify-end items-center m-5 px-5 rounded-sm shadow-md">
 
-                            <p class="p-10 text-slate-700">
-                                Tienes alguna consulta sobre mi trabajo o quieres realizar una colaboracion,
-                                te dejo este formulario para que me hagas llegar cualquuier consulta con respecto
-                                a lo que hago. Gracias y saludos.
-                            </p>
-
-                            <div class="text-slate-600 font-medium text-center mb-4">
+                            <div class=" mb-4">
                                 <div>
-                                    <ul class="text-center text-sm space-y-1">
+                                    <ul class="text-center text-sm space-y-1 text-slate-500 font-semibold whitespace-nowrap">
                                         <li>
-                                            fabriciorivera@gmail.com
+                                            FabricioRivera2012@gmail.com
                                         </li>
                                         <li>
                                             www.linkedin.com/in/fabricio-rivera/
                                         </li>
                                         <li>
-                                            www.twitter.com
+                                            www.github.com/FabricioRivera2021
                                         </li>
                                         <li>
                                             Montevideo, Uruguay.
@@ -45,8 +38,16 @@
                             <div>
                                 <h1 class="text-xl font-semibold text-slate-500">@lang('messages.contact-title')</h1>
                             </div>
-                            <x-input :placeholder="trans('messages.contact-name')" id="name" name="name"/>
-                            <x-input type="email" placeholder="Email" id="email" name="email"/>
+                            <div class="flex w-full justify-between space-x-4">
+                                <div class="w-full space-y-4">
+                                    <x-input :placeholder="trans('messages.contact-name')" id="name" name="name"/>
+                                    <x-input type="email" placeholder="Email" id="email" name="email"/>
+                                </div>
+                                <div class="w-full space-y-4">
+                                    <x-input placeholder="Organización" id="name" name="name"/>
+                                    <x-input placeholder="Asunto" id="name" name="name"/>
+                                </div>
+                            </div>
                             <textarea 
                             class="w-full pr-8 rounded-sm border-0 py-1.5 px-2.5 text-sm ring-1 ring-slate-300 text-slate-400 focus:ring-2" 
                             name="msg"
