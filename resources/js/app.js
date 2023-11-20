6b197f55e317d311d1339a2dd55339a4c0ad773c
Rivera TI <ti_rivera@hpolicial.local>
2023-11-20 16:37:58 -0300

    añadiendo mardown para posts

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,19 @@
+
 import './bootstrap';
 import.meta.glob([
     '../images/**',
     '../fonts/**',
     '../files/**',
   ]);
+  
+  import Editor from '@toast-ui/editor'
+  import '@toast-ui/editor/dist/toastui-editor.css';
+
+// Initialize the editor
+const editor = new Editor({
+    el: document.querySelector('#editor'),
+    initialEditType: 'markdown',
+    previewStyle: 'vertical',
+    height: '600px',
+    usageStatistics: false, // Optional: Disable tracking
+});
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,33 +1,33 @@
 <x-layout>
     <x-navbar />
-        <x-card class="rounded-md shadow-md pt-10 flex items-center justify-center h-screen">
-            
-            <form enctype="multipart/form-data" action="{{route('posts.store', app()->getLocale())}}" method="POST" class="flex flex-col gap-4 items-center justify-center bg-slate-300 p-5 shadow-md rounded-lg w-[50rem]">
-                @csrf
+        <x-card class="rounded-md shadow-md flex items-center justify-center">
+            <div class="mt-[5rem]">
 
-                <h3 class="font-semibold text-slate-500 text-lg">NEW POST</h3>
-                <label for="title" class="w-full text-slate-600">Title
-                    <input name="title" type="text" class="bg-slate-50 w-full rounded-sm" >
-                </label>
-                <label for="subTitle" class="w-full text-slate-600">SubTitle
-                    <input name="subTitle" type="text" class="bg-slate-50 w-full rounded-sm" >
-                </label>
-                <label for="body" class="w-full flex flex-col text-slate-600">Body
-                    <textarea name="body" id="body" cols="30" rows="10" class="rounded-sm p-2"></textarea>
-                </label>
-                <label for="img" class="w-full flex flex-col text-slate-600">Load image
-                    <input type="file" name="img" id="img">
-                </label>
-                <label for="lang" class="self-start">Lang: 
-                    <input type="radio" name="lang" id="es" value="es" class="ml-1">ES
-                    <input type="radio" name="lang" id="en" value="en">EN
-                </label>
-                <input type="hidden" name="author" value="guest">
-                <button class="bg-blue-600 text-slate-100 px-3 py-1 self-start shadow-sm rounded-sm">Guardar</button>
-            </form>
-
-
-            @if (session('error'))
+                <form enctype="multipart/form-data" action="{{route('posts.store', app()->getLocale())}}" method="POST" class="flex flex-col gap-4 items-center justify-center p-5 w-[65rem]">
+                    @csrf
+                    <h3 class="font-semibold text-slate-500 text-lg">NEW POST</h3>
+                    <label for="title" class="w-full text-slate-600">Title
+                        <input name="title" type="text" class="bg-slate-50 w-full rounded-sm" >
+                    </label>
+                    <label for="subTitle" class="w-full text-slate-600">SubTitle
+                        <input name="subTitle" type="text" class="bg-slate-50 w-full rounded-sm" >
+                    </label>
+                    <div class="flex flex-col space-y-2 w-full">
+                        <label for="editor" class="text-gray-600 font-semibold">Content</label>
+                        <div id="editor" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm bg-slate-100"></div>
+                    </div>
+                    <label for="img" class="w-full flex flex-col text-slate-600">Load image
+                        <input type="file" name="img" id="img">
+                    </label>
+                    <label for="lang" class="self-start">Lang: 
+                        <input type="radio" name="lang" id="es" value="es" class="ml-1">ES
+                        <input type="radio" name="lang" id="en" value="en">EN
+                    </label>
+                    <input type="hidden" name="author" value="guest">
+                    <button class="bg-blue-600 text-slate-100 px-3 py-1 self-start shadow-sm rounded-sm">Guardar</button>
+                </form>
+                
+                @if (session('error'))
                 <div role="alert" 
                     class="mb-8 rounded-md border-l-4 border-green-300 bg-green-100 p-4 text-green-700 opacity-75">
                     <p class="font-bold">
@@ -35,7 +35,8 @@
                     </p>
                     <p>{{ session('success') }}</p>
                 </div>
-            @endif
+                @endif
+            </div>
         </x-card>
     <x-footer />
 </x-layout>
