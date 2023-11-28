9817b0378d97224d0c9120c252887bbe8bd405f0
Fabricio Rivera <fabricio.rivera2012@gmail.com>
2023-11-28 13:18:07 -0300

    add edit posts #3

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -3,23 +3,43 @@
         <x-card class="rounded-md shadow-md flex items-center justify-center">
             <div class="mt-[5rem]">
 
-                <form id="form" enctype="multipart/form-data" action="{{ route('posts.update', ['post' => $post]) }}" method="POST" class="flex flex-col gap-4 items-center justify-center py-5 w-[65rem]">
+                <form id="form" enctype="multipart/form-data" action="{{ route('posts.update', [ 'locale' => app()->getLocale() , $post ]) }}" method="POST" class="flex flex-col gap-4 items-center justify-center py-5 w-[65rem]">
                     @csrf
                     @method('PUT')
                     <h3 class="font-semibold text-slate-500 text-lg">EDIT POST</h3>
                     <label for="title" class="w-full text-slate-600">Title
                         <input name="title" type="text" class="bg-slate-50 w-full rounded-sm" value="{{$post->title}}">
+                        @error('title')
+                        <div class="mt-1 text-xs text-red-500">
+                            {{ $message }}
+                        </div>
+                        @enderror
                     </label>
                     <label for="subTitle" class="w-full text-slate-600">SubTitle
                         <input name="subTitle" type="text" class="bg-slate-50 w-full rounded-sm" value="{{$post->subTitle}}">
+                        @error('subTitle')
+                        <div class="mt-1 text-xs text-red-500">
+                            {{ $message }}
+                        </div>
+                        @enderror
                     </label>
                     <div class="flex flex-col space-y-2 w-full">
                         <label for="editor" class="text-gray-600 font-semibold">Content</label>
-                        <div id="editor" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm bg-slate-100">{{$post->body}}</div>
+                        <div id="editor" name="editor" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm bg-slate-100">{{$post->body}}</div>
+                        @error('editor')
+                        <div class="mt-1 text-xs text-red-500">
+                            {{ $message }}
+                        </div>
+                        @enderror
                     </div>
                     <input type="hidden" name="body" id="body">
                     <label for="img" class="text-sm self-start text-slate-100 flex flex-col hover:cursor-pointer bg-blue-600 px-3 py-0.5 rounded-sm items-center shadow-sm hover:bg-slate-200 hover:text-slate-600">Load Thumbnail
                         <input class="hidden" type="file" name="img" id="img">
+                        @error('img')
+                        <div class="mt-1 text-xs text-red-500">
+                            {{ $message }}
+                        </div>
+                        @enderror
                     </label>
                     <label for="lang" class="self-start">Lang: 
                         <input class="text-xs" type="radio" name="lang" id="es" value="es" class="ml-1" {{($post->lang == 'es') ? 'checked' : ''}}>es
@@ -39,16 +59,6 @@
                 <div>
                     <i id="imageUrl"></i>
                 </div>
-                
-                @if (session('error'))
-                <div role="alert" 
-                    class="mb-8 rounded-md border-l-4 border-green-300 bg-green-100 p-4 text-green-700 opacity-75">
-                    <p class="font-bold">
-                        Error!!
-                    </p>
-                    <p>{{ session('error') }}</p>
-                </div>
-                @endif
             </div>
         </x-card>
     <x-footer />
