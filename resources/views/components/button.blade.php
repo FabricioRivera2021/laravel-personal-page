9ef63abdf7d0ee61ff8dbb1ac0010f0f9eafa128
Fabricio Rivera <fabricio.rivera2012@gmail.com>
2024-03-04 17:38:53 -0300

    arreglando cosas en proyectos y agregando animaciones basicas

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->class(["bg-blue-400 hover:bg-slate-600 text-sm hover:text-slate-200 text-slate-200 rounded-md border py-2 px-4 font-semibold"]) }}>
+<button {{ $attributes->class(["bg-blue-400 hover:bg-slate-600 text-sm hover:text-slate-200 text-slate-200 rounded-md border py-2 px-4 font-semibold transition hover:scale-105"]) }}>
     {{$slot}}
 </button>
--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -2,9 +2,15 @@
 
     <x-navbar />
 
-    <div class="w-full">
-        <div class="flex flex-col bg-slate-100">
-            {{-- !? PROYECTO CARD --}}
+    <div class="pt-20 w-full bg-slate-500">
+        <h2 class="text-center text-4xl font-semibold">
+            Proyectos
+        </h2>
+    </div>
+
+    <div class="max-w-7xl grid sm:grid-cols-3 grid-cols-1 mx-auto py-20 gap-5 bg-slate-100">
+        {{-- <div class="flex flex-col bg-slate-100">
+            !? PROYECTO CARD
             <div class="max-w-xl md:max-w-7xl justify-self-center self-center md:pt-12">
                 <h2 class="font-semibold uppercase text-slate-600 mt-20 ml-5 mb-5 text-xl">@lang('messages.project-1-title') <a class="text-sm font-light underline text-blue-400" href="https://fabriciorivera.art" target="_blank" >@lang('messages.view-site')</a> </h2>
                 <div class="flex flex-col md:flex-row justify-center items-center md:space-x-20">
@@ -44,11 +50,123 @@
                 <div class="shadow-md">
                     <img class="rounded-md" src="{{Vite::asset('resources/images/screenshots/photography/contact.jpg')}}" alt="">
                 </div>
+            </div> --}}
+            {{-- !? PROYECTO CARD --}}
+            <div class="items-center p-5 col-span-1 hover:bg-slate-100 shadow-sm hover:shadow-xl hover:border-t-1 transition ease-out hover:-translate-y-5 hover:bg-blend-darken hover:scale-105 rounded">
+                <div>
+                    <div class="flex flex-col md:flex-row justify-center items-center md:space-x-20">
+                        <div>
+                            <div class="flex flex-col items-start mb-4">
+                                <h3 class="text-blue-500 font-semibold mb-2 whitespace-nowrap">Fabricio Rivera Fotografia</h3>
+                                <div class="flex space-x-3">
+                                    <p class="text-xs border rounded-3xl bg-blue-500 px-3 py-0.5 text-slate-100 shadow-md">React</p>
+                                    <p class="text-xs border rounded-3xl bg-sky-700 px-3 py-0.5 text-slate-100 shadow-md whitespace-nowrap">Tailwind CSS</p>
+                                    <p class="text-xs border rounded-3xl bg-amber-600 px-3 py-0.5 text-slate-100 shadow-md whitespace-nowrap">AWS</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="grid grid-cols-2 mx-auto gap-5 p-5">
+                    <div class="col-span-2 place-self-center rounded-sm shadow-md">
+                        <img class="rounded-md" src="{{Vite::asset('resources/images/screenshots/photography/main.jpg')}}" alt="">
+                    </div>
+                    <div class="shadow-md">
+                        <img class="rounded-md" src="{{Vite::asset('resources/images/screenshots/photography/portfolio.jpg')}}" alt="">
+                    </div>
+                    <div class="shadow-md">
+                        <img class="rounded-md" src="{{Vite::asset('resources/images/screenshots/photography/portfolio1.jpg')}}" alt="">
+                    </div>
+                    {{-- <div class="col-span-2 place-self-center rounded-sm shadow-md">
+                        <img class="rounded-md" src="{{Vite::asset('resources/images/screenshots/photography/portfolio2.jpg')}}" alt="">
+                    </div>
+                    <div class="shadow-md">
+                        <img class="rounded-md" src="{{Vite::asset('resources/images/screenshots/photography/portfolio3.jpg')}}" alt="">
+                    </div>
+                    <div class="shadow-md">
+                        <img class="rounded-md" src="{{Vite::asset('resources/images/screenshots/photography/contact.jpg')}}" alt="">
+                    </div> --}}
+                </div>
             </div>
+            <div class="items-center p-5 col-span-1 hover:bg-slate-100 shadow-sm hover:shadow-xl hover:border-t-1 transition ease-out hover:-translate-y-5 hover:bg-blend-darken hover:scale-105 rounded">
+                <div>
+                    <div class="flex flex-col md:flex-row justify-center items-center md:space-x-20">
+                        <div>
+                            <div class="flex flex-col items-start mb-4">
+                                <h3 class="text-blue-500 font-semibold mb-2 whitespace-nowrap">Fabricio Rivera Fotografia</h3>
+                                <div class="flex space-x-3">
+                                    <p class="text-xs border rounded-3xl bg-blue-500 px-3 py-0.5 text-slate-100 shadow-md">React</p>
+                                    <p class="text-xs border rounded-3xl bg-sky-700 px-3 py-0.5 text-slate-100 shadow-md whitespace-nowrap">Tailwind CSS</p>
+                                    <p class="text-xs border rounded-3xl bg-amber-600 px-3 py-0.5 text-slate-100 shadow-md whitespace-nowrap">AWS</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="grid grid-cols-2 mx-auto gap-5 p-5">
+                    <div class="col-span-2 place-self-center rounded-sm shadow-md">
+                        <img class="rounded-md" src="{{Vite::asset('resources/images/screenshots/photography/main.jpg')}}" alt="">
+                    </div>
+                    <div class="shadow-md">
+                        <img class="rounded-md" src="{{Vite::asset('resources/images/screenshots/photography/portfolio.jpg')}}" alt="">
+                    </div>
+                    <div class="shadow-md">
+                        <img class="rounded-md" src="{{Vite::asset('resources/images/screenshots/photography/portfolio1.jpg')}}" alt="">
+                    </div>
+                    {{-- <div class="col-span-2 place-self-center rounded-sm shadow-md">
+                        <img class="rounded-md" src="{{Vite::asset('resources/images/screenshots/photography/portfolio2.jpg')}}" alt="">
+                    </div>
+                    <div class="shadow-md">
+                        <img class="rounded-md" src="{{Vite::asset('resources/images/screenshots/photography/portfolio3.jpg')}}" alt="">
+                    </div>
+                    <div class="shadow-md">
+                        <img class="rounded-md" src="{{Vite::asset('resources/images/screenshots/photography/contact.jpg')}}" alt="">
+                    </div> --}}
+                </div>
+            </div>
+            <div class="items-center p-5 col-span-1 hover:bg-slate-100 shadow-sm hover:shadow-xl hover:border-t-1 transition ease-out hover:-translate-y-5 hover:bg-blend-darken hover:scale-105 rounded">
+                <div>
+                    <div class="flex flex-col md:flex-row justify-center items-center md:space-x-20">
+                        <div>
+                            <div class="flex flex-col items-start mb-4">
+                                <h3 class="text-blue-500 font-semibold mb-2 whitespace-nowrap">Fabricio Rivera Fotografia</h3>
+                                <div class="flex space-x-3">
+                                    <p class="text-xs border rounded-3xl bg-blue-500 px-3 py-0.5 text-slate-100 shadow-md">React</p>
+                                    <p class="text-xs border rounded-3xl bg-sky-700 px-3 py-0.5 text-slate-100 shadow-md whitespace-nowrap">Tailwind CSS</p>
+                                    <p class="text-xs border rounded-3xl bg-amber-600 px-3 py-0.5 text-slate-100 shadow-md whitespace-nowrap">AWS</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="grid grid-cols-2 mx-auto gap-5 p-5">
+                    <div class="col-span-2 place-self-center rounded-sm shadow-md">
+                        <img class="rounded-md" src="{{Vite::asset('resources/images/screenshots/photography/main.jpg')}}" alt="">
+                    </div>
+                    <div class="shadow-md">
+                        <img class="rounded-md" src="{{Vite::asset('resources/images/screenshots/photography/portfolio.jpg')}}" alt="">
+                    </div>
+                    <div class="shadow-md">
+                        <img class="rounded-md" src="{{Vite::asset('resources/images/screenshots/photography/portfolio1.jpg')}}" alt="">
+                    </div>
+                    {{-- <div class="col-span-2 place-self-center rounded-sm shadow-md">
+                        <img class="rounded-md" src="{{Vite::asset('resources/images/screenshots/photography/portfolio2.jpg')}}" alt="">
+                    </div>
+                    <div class="shadow-md">
+                        <img class="rounded-md" src="{{Vite::asset('resources/images/screenshots/photography/portfolio3.jpg')}}" alt="">
+                    </div>
+                    <div class="shadow-md">
+                        <img class="rounded-md" src="{{Vite::asset('resources/images/screenshots/photography/contact.jpg')}}" alt="">
+                    </div> --}}
+                </div>
+            </div>
+
+
+
 
             {{-- ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- --}}
             {{-- !? PROYECTO CARD --}}
-            <div class="max-w-7xl  justify-self-center self-center md:pt-12">
+            {{-- <div class="max-w-7xl  justify-self-center self-center md:pt-12">
                 <h2 class="font-semibold uppercase text-slate-600 mt-20 ml-5 mb-5 text-xl">@lang('messages.project-2-title') <a class="text-sm font-light underline text-blue-400" href="{{ route('legacy', app()->getLocale() )}}" target="_blank">@lang('messages.view-site')</a> </h2>
                 <div class="flex flex-col md:flex-row justify-center items-center md:space-x-20">
                     <div>
@@ -82,10 +200,8 @@
                 <div class="col-span-2 place-self-center rounded-sm shadow-md">
                     <img class="rounded-md" src="{{Vite::asset('resources/images/screenshots/old-personal-page/mobile.jpg')}}" alt="">
                 </div>
-            </div>
+            </div> --}}
 
-
-        </div>
     </div>
 
 </x-layout>
